beb1906dfac8b102827c14d483ac2eb166a3eb8e
ahmedaltamas2@gmail.com <ahmedaltamas2@gmail.com>
2024-07-03 23:53:42 +0530

    first commit

--- src/app/_components/footer.js
+++ src/app/_components/footer.js
@@ -9,28 +9,22 @@ export default function Footer() {
       <div className="row">
         <div className="col-md-4" style={{marginTop:"0.6rem"}}>
           <div className="footer-logo">
-            <img src="https://assets.inshorts.com/website_assets/images/logo_footer.png" alt="Inshorts Logo" />
           </div>
         </div>
         <div className="col-md-4">
-          <Link href="/contact"  className="footer-logo"><img src="https://assets.inshorts.com/website_assets/images/contact_icon.png"></img></Link>
+          <Link href="/contact"  className="footer-logo"></Link>
         </div>
         <div className="col-md-4 text-center">
           <div className="social-icons">
-            <Link href="https://facebook.com" target="_blank" className="text-white" style={{margin: "0 10px",  fontSize: "1.8em"}}><i className="fab fa-facebook-f"></i></Link>
-            <Link href="https://twitter.com" target="_blank" className="text-white" style={{margin: "0 10px",  fontSize: "1.8em"}}><i className="fab fa-twitter"></i></Link>
-            <Link href="https://linkedin.com" target="_blank" className="text-white" style={{margin: "0 10px",  fontSize: "1.8em"}}><i className="fab fa-linkedin-in"></i></Link>
+          <Link href="https://leetcode.com/u/tasbiya/" target="_blank" className="text-white" style={{margin: "0 10px",  fontSize: "1.8em"}}><i class="fab fa-hackerrank"></i></Link>
+            <Link href="https://github.com/tasbiya02" target="_blank" className="text-white" style={{margin: "0 10px",  fontSize: "1.8em"}}><i className="fab fa-github"></i></Link>
+            <Link href="https://www.linkedin.com/in/tasbiya-21ba2a23a/" target="_blank" className="text-white" style={{margin: "0 10px",  fontSize: "1.8em"}}><i className="fab fa-linkedin-in"></i></Link>
           </div>
         </div>
       </div>
       <div className="row mt-3 ">
         <div className="col-md-4">
-          <p>Inshorts <span style={{fontWeight:"150"}}>Pte. Ltd. &copy;COPYRIGHT 2024</span></p>
-        </div>
-        <div className="col-md-4" style={{marginLeft:"0.7rem"}}>
-          <Link href="/terms" style={{textDecoration:"none",fontWeight:"150"}} className="text-white"> Terms & conditions</Link>
-          <br />
-          <Link href="/privacy" style={{textDecoration:"none",fontWeight:"150"}} className="text-white"> Privacy Policies</Link>
+          <p>tasbiya.news.in <span style={{fontWeight:"150"}}>Pte. Ltd. &copy;COPYRIGHT 2024</span></p>
         </div>
       </div>
     </div>
--- src/app/_components/navbar.js
+++ src/app/_components/navbar.js
@@ -1,16 +1,15 @@
 import Link from "next/link";
 
-
 export default function Navbar() {
   return (
-   <>
-    <nav className="navbar bg-body-tertiary" >
-  <div className="container nav-img" style={{display:"flex",alignItems:"center",justifyContent:"center"}}>, 
-    <Link className="navbar-brand nav-brand" href="/news">
-      <img src="https://assets.inshorts.com/website_assets/images/logo_inshorts.png" alt="Bootstrap" width="150" height="70"/>
-    </Link>
-  </div>
-   </nav>
-   </>
-  )
+    <>
+      <nav className="navbar bg-body-tertiary">
+        <div className="container nav-img" style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
+          <Link className="navbar-brand nav-brand" href="/news">
+            <span style={{fontWeight: "bold", fontSize: "24px"}}>tasbiya.news.in</span>
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
 }
